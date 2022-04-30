5743055cf75e642d2875a0c339716dad357b2d1a
Mehrdad Shokri <code.poet95@gmail.com>
2022-04-30 19:29:23 +0430

    setup firebase

---
+++ lib/rx/services/firebase_service.dart
@@ -0,0 +1,19 @@
+import 'package:client/firebase_options.dart';
+import 'package:client/rx/services/rx_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseService extends RxService {
+  late FirebaseApp firebaseApp;
+
+  @override
+  Future<void> onCreate() async {
+    firebaseApp = await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform)
+  }
+
+  @override
+  Future<void> onTerminate() async {
+    firebaseApp.delete();
+  }
+
+}

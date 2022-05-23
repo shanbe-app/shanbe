f562c9d66261105418f613840192f7641401e30b
Mehrdad Shokri <code.poet95@gmail.com>
2022-05-23 13:42:30 +0430

    rerun amplify

--- lib/models/ModelProvider.dart
+++ lib/models/ModelProvider.dart
@@ -20,12 +20,14 @@
 // ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
 
 import 'package:amplify_core/amplify_core.dart';
+import 'Profile.dart';
 import 'Project.dart';
 import 'Reminder.dart';
 import 'Tag.dart';
 import 'Todo.dart';
 import 'TodoTag.dart';
 
+export 'Profile.dart';
 export 'Project.dart';
 export 'Reminder.dart';
 export 'ReminderStatus.dart';
@@ -36,9 +38,9 @@ export 'TodoTag.dart';
 
 class ModelProvider implements ModelProviderInterface {
   @override
-  String version = "09931d68ca0017ac28370179ec2c9edb";
+  String version = "b03640eb54515f78bcd9daea686272c6";
   @override
-  List<ModelSchema> modelSchemas = [Project.schema, Reminder.schema, Tag.schema, Todo.schema, TodoTag.schema];
+  List<ModelSchema> modelSchemas = [Profile.schema, Project.schema, Reminder.schema, Tag.schema, Todo.schema, TodoTag.schema];
   static final ModelProvider _instance = ModelProvider();
   @override
   List<ModelSchema> customTypeSchemas = [];
@@ -47,6 +49,8 @@ class ModelProvider implements ModelProviderInterface {
   
   ModelType getModelTypeByModelName(String modelName) {
     switch(modelName) {
+      case "Profile":
+        return Profile.classType;
       case "Project":
         return Project.classType;
       case "Reminder":

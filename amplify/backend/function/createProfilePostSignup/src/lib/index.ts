c5f6887ecc09f9fa8e499d76039a68d77d3e7db5
Mehrdad Shokri <code.poet95@gmail.com>
2022-05-24 12:59:39 +0430

    wip

--- amplify/backend/function/createProfilePostSignup/src/lib/index.ts
+++ amplify/backend/function/createProfilePostSignup/src/lib/index.ts
@@ -1,13 +1,29 @@
-import { Context, APIGatewayProxyCallback, APIGatewayEvent } from 'aws-lambda';
+import {Context, APIGatewayProxyCallback, APIGatewayEvent} from 'aws-lambda';
 import * as AWS from 'aws-sdk';
+import {DocumentClient} from "aws-sdk/clients/dynamodb";
 
+AWS.config.update({region: process.env.TABLE_REGION});
+const dynamodb: DocumentClient = new AWS.DynamoDB.DocumentClient();
+let tableName = "";
+if (process.env.ENV && process.env.ENV !== "NONE") {
+    tableName = tableName + '-' + process.env.ENV;
+}
 export const lambdaHandler = (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback): void => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-    callback(null, {
-        statusCode:200,
-        body: JSON.stringify({
-            message: 'hello world',
-        }),
-    });
+    dynamodb.put({TableName: tableName, Item: {premium_until: null}}, (err, data) => {
+        if (err)
+            callback(err, {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'hello world',
+                }),
+            })
+        callback(err, {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'User profile created successfully.',
+            }),
+        })
+    })
 };

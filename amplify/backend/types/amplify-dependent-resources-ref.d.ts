9bcdcdc78084fb40f31702a780a52edda41f1cd4
Mehrdad Shokri <code.poet95@gmail.com>
2022-05-25 06:53:44 +0430

    update amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -24,5 +24,13 @@ export type AmplifyDependentResourcesAttributes = {
             "BucketName": "string",
             "Region": "string"
         }
+    },
+    "function": {
+        "createProfilePostSignup": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
     }
 }

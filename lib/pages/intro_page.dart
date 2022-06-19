2adadbd4da6d4241b3f0e0519d858bc3e29b124e
Mehrdad Shokri <code.poet95@gmail.com>
2022-06-19 16:34:09 +0430

    wip setting up intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -30,12 +30,12 @@ class _IntroPageState extends State<IntroPage> {
     appIntroData = [
       AppIntroData(t.appIntroTitle1, 'assets/files/meditate3.json',
           t.appIntroDescription1),
-      AppIntroData(t.appIntroTitle2, 'assets/files/yoga2.json',
-          t.appIntroDescription2),
       AppIntroData(
-          t.appIntroTitle3, 'assets/files/calendar.json', t.appIntroDescription3),
-      AppIntroData(t.appIntroTitle4, 'assets/files/book.json',
-          t.appIntroDescription4),
+          t.appIntroTitle2, 'assets/files/yoga2.json', t.appIntroDescription2),
+      AppIntroData(t.appIntroTitle3, 'assets/files/calendar.json',
+          t.appIntroDescription3),
+      AppIntroData(
+          t.appIntroTitle4, 'assets/files/book.json', t.appIntroDescription4),
     ];
   }
 
@@ -43,7 +43,8 @@ class _IntroPageState extends State<IntroPage> {
   Widget build(BuildContext context) {
     return PlatformScaffold(
       iosContentPadding: true,
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: Constants.PAGE_PADDING,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
@@ -63,58 +64,71 @@ class _IntroPageState extends State<IntroPage> {
             const SizedBox(
               height: 32,
             ),
-            Container(
-              height: 520,
-              child: PageView(
-                controller: _controller,
-                children: appIntroData
-                    .map((e) => Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Lottie.asset(e.lottieDir,
-                                alignment: Alignment.center,
-                                repeat: true,
-                                reverse: false,
-                                width: double.maxFinite,
-                                fit: BoxFit.cover),
-                            const SizedBox(
-                              height: 40,
-                            ),
-                            Text(
-                              e.title,
-                              style: TextStyle(
-                                  color: headingColor(context),
-                                  fontWeight: Constants.MEDIUM_FONT_WEIGHT,
-                                  fontSize: Constants.H6_FONT_SIZE),
-                            ),
-                            const SizedBox(
-                              height: 16,
-                            ),
-                            Text(e.description,
-                                textAlign: TextAlign.center,
+            Flexible(
+                flex: 10,
+                child: PageView(
+                  controller: _controller,
+                  children: appIntroData
+                      .map((e) => Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Expanded(
+                                  child: Lottie.asset(e.lottieDir,
+                                      alignment: Alignment.center,
+                                      repeat: true,
+                                      reverse: false,
+                                      width: double.maxFinite,
+                                      fit: BoxFit.contain)),
+                              const SizedBox(
+                                height: 24,
+                              ),
+                              Text(
+                                e.title,
                                 style: TextStyle(
-                                    color: secondaryTextColor(context),
-                                    fontWeight: Constants.REGULAR_FONT_WEIGHT,
-                                    fontSize: Constants.S2_FONT_SIZE))
-                          ],
-                        ))
-                    .toList(),
-              ),
-            ),
+                                    color: headingColor(context),
+                                    fontWeight: Constants.MEDIUM_FONT_WEIGHT,
+                                    fontSize: Constants.H6_FONT_SIZE),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Text(e.description,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: secondaryTextColor(context),
+                                      fontWeight: Constants.REGULAR_FONT_WEIGHT,
+                                      fontSize: Constants.S2_FONT_SIZE))
+                            ],
+                          ))
+                      .toList(),
+                )),
             const SizedBox(
               height: 32,
             ),
-            SmoothPageIndicator(
-              controller: _controller,
-              count: appIntroData.length,
-              textDirection:
-                  isRtl(context) ? TextDirection.rtl : TextDirection.ltr,
-              effect: WormEffect(
-                  dotWidth: 8,
-                  dotHeight: 8,
-                  dotColor: placeholderColor(context),
-                  activeDotColor: Constants.PRIMARY_COLOR),
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  SmoothPageIndicator(
+                    controller: _controller,
+                    count: appIntroData.length,
+                    textDirection:
+                        isRtl(context) ? TextDirection.rtl : TextDirection.ltr,
+                    effect: WormEffect(
+                        dotWidth: 12,
+                        dotHeight: 12,
+                        dotColor: placeholderColor(context),
+                        activeDotColor: Constants.PRIMARY_COLOR),
+                  ),
+                  Spacer(),
+                  PlatformElevatedButton(
+                    child: Text('Let\'s go'),
+                    onPressed: (){},
+                  )
+                ],
+              ),
             ),
+            SizedBox(height: 32,)
           ],
         ),
       ),
